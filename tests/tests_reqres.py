f4fdb2579a63abb93b3f15f339301690e213bb57
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-09-14 00:12:22 +0700

    working

--- tests/tests_reqres.py
+++ tests/tests_reqres.py
@@ -1,7 +1,7 @@
 import requests
 from selene import browser
 
-endpoint = ['/api/users?page=2/', '/api/users/2', '/api/users/23']
+endpoint = ('/api/users?page=2/', '/api/users/2', '/api/users/23')
 
 #@pytest.fixture
 #def browser_link(scope="session", autouse=True):
@@ -13,8 +13,9 @@ endpoint = ['/api/users?page=2/', '/api/users/2', '/api/users/23']
 def tests_pozitive():
     url = 'https://reqres.in/'
     for point in endpoint:
-        response = requests.get(url, params={'q': endpoint})
-        response.raise_for_status()
-        print(response)
+        r = requests.get(url, params={'q': 'endpoint'})
+        #r.status_code == r.raise_for_status()
+        #r.raise_for_status()
+        print(r)
         
     
