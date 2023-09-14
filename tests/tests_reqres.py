d8ee6b9c33dab1ee4a1d507ab4a8e0931a93bbbf
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-09-14 23:07:21 +0700

    working

--- tests/tests_reqres.py
+++ tests/tests_reqres.py
@@ -1,36 +1,35 @@
 import pytest
 from selene import browser, be, have, command
 
-@pytest.fixture
-def browser_link(scope="session"):
-    browser.config.base_url = "https://reqres.in/"
-
-    yield
-
-    browser.quit()
+#@pytest.fixture
+#def browser_link(scope="session"):
+#    browser.config.base_url = "https://reqres.in/"
+#
+#    yield
+#
+#    browser.quit()
     
-def test_pozitive(browser_link):
-    browser.open('/')
-
-    browser.element(".console .endpoints li").perform(command.js.scroll_into_view)
+def test_pozitive():
+    browser.open('https://reqres.in/')
+    browser.element('class="active"').perform(command.js.scroll_into_view)
     #browser.element(".li[@class='active']").should(have.text('response-code, 200'))
 
 
 
 
 
-    #if r.status_code == 400:
-    #    print((r.status_code)'Верно')
-    #else:
-    #    print(r.status_code)
 
 
 
 
 
 
-        #print(data)
-        #r.status_code == r.raise_for_status()
-        #r.raise_for_status()
+
+
+
+
+
+
+
 
 
