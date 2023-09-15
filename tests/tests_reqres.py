8b3e10d70040d6304410c81040881db725de5027
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-09-15 22:11:02 +0700

    working

--- tests/tests_reqres.py
+++ tests/tests_reqres.py
@@ -1,24 +1,25 @@
 import pytest, requests
 from selene import browser, be, have, command
 
-@pytest.fixture
-def browser_link(scope="function", autouse=True):
-    url = browser.config.base_url = "https://reqres.in/"
+#@pytest.fixture
+#def browser_link(scope="function"):
+#    browser.config.base_url = "https://reqres.in/"
+#    browser.config.timeout = 7.0
+#    browser.config.hold_driver_at_exit = True
+#    browser.config.window_width = 1920
+#    browser.config.window_height = 1080
 
-    yield
-
-    browser.quit()
-    
-def test_pozitive(browser_link):
-    browser.open('url')
-    browser.element('[data-http="get"]').perform(command.js.scroll_into_view).click()
-    response = browser.element('[class="response"]').should(have.text('200'))
-    if response == 200:
-        print(response)
+def test_pozitive():
+    browser.open('https://reqres.in')
+    browser.element("li[data-id='users-single-not-found']").perform(command.js.scroll_into_view).click()
+    browser.config.hold_driver_at_exit = True
+    #response = browusersve.text('200'))
+    #if response == 200:
+    #    print(response)
 
 
 
-
+    #
 
 
 
