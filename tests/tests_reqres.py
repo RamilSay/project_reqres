c239d41d733753e7047a97acaaa3f5f20554eb38
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-09-15 20:51:16 +0700

    working

--- tests/tests_reqres.py
+++ tests/tests_reqres.py
@@ -1,19 +1,20 @@
 import pytest, requests
 from selene import browser, be, have, command
 
-#@pytest.fixture
-#def browser_link(scope="session"):
-#    browser.config.base_url = "https://reqres.in/"
-#
-#    yield
-#
-#    browser.quit()
+@pytest.fixture
+def browser_link(scope="function", autouse=True):
+    url = browser.config.base_url = "https://reqres.in/"
+
+    yield
+
+    browser.quit()
     
-def test_pozitive():
-    browser.open('https://reqres.in/')
+def test_pozitive(browser_link):
+    browser.open('url')
     browser.element('[data-http="get"]').perform(command.js.scroll_into_view).click()
-    browser.element('[class="response"]').should(have.text('200'))
-    assert browser.element('status_code == 200')
+    response = browser.element('[class="response"]').should(have.text('200'))
+    if response == 200:
+        print(response)
 
 
 
