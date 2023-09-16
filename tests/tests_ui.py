fedaab84fc8bfef906ef21daa41723b87deba58d
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-09-16 12:24:30 +0700

    working

--- tests/tests_ui.py
+++ tests/tests_ui.py
@@ -6,7 +6,6 @@ response = " Статус кода соответствует запросу"
 #UI тесты сайта
 def test_good(browser_setup):
     browser.open(url)
-    # browser.config.hold_driver_at_exit = True
     browser.element("li[data-id='users']").perform(command.js.scroll_into_view).click()
     browser.element(".response-code").should(have.text(code))
     print(response)
@@ -18,3 +17,6 @@ def test_negative(browser_setup):
     browser.element(".response-code.bad").should(have.text(status_code))
     print(response)
 
+
+
+# browser.config.hold_driver_at_exit = True
