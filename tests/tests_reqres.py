e2b71e785f41116544d427c843e1c4c478211bec
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-09-16 12:10:31 +0700

    working

--- tests/tests_reqres.py
+++ tests/tests_reqres.py
@@ -1,28 +1,22 @@
-import pytest
-from selene import browser, be, have, command
+from selene import browser, have, command
+
+url = 'https://reqres.in/'
 
 
-@pytest.fixture
-def link():
-    browser.config.base_url = 'https://reqres.in/'
-    browser.config.timeout = 7.0
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-
-
-def test_good(link):
-    status_code = "200"
-    browser.open('/')
-    browser.config.hold_driver_at_exit = True
+def test_good(browser_setup):
+    code = "200"
+    response = " Статус кода соответствует запросу"
+    browser.open(url)
+    # browser.config.hold_driver_at_exit = True
     browser.element("li[data-id='users']").perform(command.js.scroll_into_view).click()
-    browser.element(".response-code").should(have.text(status_code))
+    browser.element(".response-code").should(have.text(code))
+    print(response)
 
-
-def test_negative(link):
+def test_negative(browser_setup):
     status_code = "404"
-    browser.open('/')
+    browser.open(url)
     browser.element("li[data-id='users-single-not-found']").click()
-    browser.element(".response-code").should(have.text(status_code))
+    browser.element(".response-code.bad").should(have.text(status_code))
 
     # r = requests.status_codes = 200
     # if r != 200:
