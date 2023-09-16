d0225ab4223e21eae434779f0bfc4d504fee941a
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-09-16 12:23:27 +0700

    working

---
+++ tests/tests_cases.py
@@ -0,0 +1,11 @@
+
+
+def requests():
+
+# r = requests.status_codes = 200
+    # if r != 200:
+    #    print(f'Код верный', r)
+    # else:
+    #    print(f'Код неверный',r)
+    #
+    #    #requests.status_codes = 200
--- tests/tests_ui.py
+++ tests/tests_ui.py
@@ -1,11 +1,10 @@
 from selene import browser, have, command
 
 url = 'https://reqres.in/'
-
-#UI tests
+code = "200"
+response = " Статус кода соответствует запросу"
+#UI тесты сайта
 def test_good(browser_setup):
-    code = "200"
-    response = " Статус кода соответствует запросу"
     browser.open(url)
     # browser.config.hold_driver_at_exit = True
     browser.element("li[data-id='users']").perform(command.js.scroll_into_view).click()
@@ -17,13 +16,5 @@ def test_negative(browser_setup):
     browser.open(url)
     browser.element("li[data-id='users-single-not-found']").click()
     browser.element(".response-code.bad").should(have.text(status_code))
+    print(response)
 
-    # r = requests.status_codes = 200
-    # if r != 200:
-    #    print(f'Код верный', r)
-    # else:
-    #    print(f'Код неверный',r)
-    #
-    #    #requests.status_codes = 200
-
-    #
