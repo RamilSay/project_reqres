36659ad611d8f6f79cc6de95b1dd6eb63de4df7f
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-09-17 21:50:27 +0700

    working

--- tests/tests_ui.py
+++ tests/tests_ui.py
@@ -2,7 +2,7 @@ from selene import browser, have, command
 
 url = 'https://reqres.in/'
 check = ("LIST USERS", "SINGLE USER", "")
-code = ("200", "201", "404", "204", "400")
+code = ("200", "201", "204", "404", "400")
 
 
 # UI тесты
@@ -10,7 +10,7 @@ def test_code_pass(browser_setup):
     browser.open(url)
     browser.element("li[data-id='users']").perform(command.js.scroll_into_view).click()
     browser.element(".response-code").should(have.exact_text(code[0]))
-    print(f"GET LIST USERS / Статус кода: {code[0]}")
+    print(f"\nGET LIST USERS / Статус кода: {code[0]}")
 
     browser.element("li[data-id='users-single']").perform(command.js.scroll_into_view).click()
     browser.element(".response-code").should(have.exact_text(code[0]))
@@ -40,10 +40,6 @@ def test_code_pass(browser_setup):
     browser.element(".response-code").should(have.exact_text(code[0]))
     print(f"POST REGISTER - SUCCESSFUL / Статус кода: {code[0]}")
 
-    browser.element("li[data-id='register-successful']").perform(command.js.scroll_into_view).click()
-    browser.element(".response-code").should(have.exact_text(code[0]))
-    print(f"POST REGISTER - SUCCESSFUL / Статус кода: {code[0]}")
-
     browser.element("li[data-id='login-successful']").perform(command.js.scroll_into_view).click()
     browser.element(".response-code").should(have.exact_text(code[0]))
     print(f"POST LOGIN - SUCCESSFUL / Статус кода: {code[0]}")
@@ -56,16 +52,16 @@ def test_code_pass(browser_setup):
 def test_negative(browser_setup):
     browser.open(url)
     browser.element("li[data-id='users-single-not-found']").click()
-    browser.element(".response-code.bad").should(have.text(code[2]))
-    print(f"GET SINGLE USER NOT FOUND / Статус кода: {code[2]}")
+    browser.element(".response-code.bad").should(have.text(code[3]))
+    print(f"\nGET SINGLE USER NOT FOUND / Статус кода: {code[3]}")
 
     browser.element("li[data-id='unknown-single-not-found']").perform(command.js.scroll_into_view).click()
-    browser.element(".response-code.bad").should(have.exact_text(code[2]))
-    print(f"GET SINGLE RESOURCE NOT FOUND / Статус кода: {code[2]}")
+    browser.element(".response-code.bad").should(have.exact_text(code[3]))
+    print(f"GET SINGLE RESOURCE NOT FOUND / Статус кода: {code[3]}")
 
     browser.element("li[data-id='delete']").perform(command.js.scroll_into_view).click()
-    browser.element(".response-code.bad").should(have.exact_text(code[3]))
-    print(f"DELETE / Статус кода: {code[3]}")
+    browser.element(".response-code.bad").should(have.exact_text(code[2]))
+    print(f"DELETE / Статус кода: {code[2]}")
 
     browser.element("li[data-id='register-unsuccessful']").perform(command.js.scroll_into_view).click()
     browser.element(".response-code.bad").should(have.exact_text(code[4]))
@@ -75,4 +71,4 @@ def test_negative(browser_setup):
     browser.element(".response-code.bad").should(have.exact_text(code[4]))
     print(f"POST LOGIN - UNSUCCESSFUL / Статус кода: {code[4]}")
 
-# browser.config.hold_driver_at_exit = True
+
