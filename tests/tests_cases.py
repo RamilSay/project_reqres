9e0480cf4aa2534e2e1d99fdf4f97282da383afc
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-09-17 20:59:49 +0700

    working

--- tests/tests_cases.py
+++ tests/tests_cases.py
@@ -1,7 +1,16 @@
+import requests
+from selene import browser
 
+url = 'https://reqres.in/'
+code = "200"
+#response = " Статус кода соответствует запросу"
 
-def requests():
-
+def browser_manage(browser_setup):
+    browser.open(url)
+def tests_requests(browser_manage):
+    requests.get('/api/users?page=2').status_code = 200
+    r = requests.status_codes = 200
+    print(r)
 # r = requests.status_codes = 200
     # if r != 200:
     #    print(f'Код верный', r)
--- tests/tests_ui.py
+++ tests/tests_ui.py
@@ -1,21 +1,35 @@
 from selene import browser, have, command
 
 url = 'https://reqres.in/'
-code = "200"
-response = " Статус кода соответствует запросу"
-#UI тесты сайта
-def test_good(browser_setup):
+check = ("LIST USERS", "SINGLE USER", "")
+code = ("200", "201", "404")
+response_code = '.response-code'
+status = ' Статус кода 200'
+# UI тесты
+def test_code_pass(browser_setup):
     browser.open(url)
     browser.element("li[data-id='users']").perform(command.js.scroll_into_view).click()
-    browser.element(".response-code").should(have.text(code))
-    print(response)
+    browser.element(response_code).should(have.exact_text(code[0]))
+    print(f"LIST USERS / Статус кода: {code[0]}")
+    browser.element("li[data-id='users-single']").perform(command.js.scroll_into_view).click()
+    browser.element(response_code).should(have.exact_text(code[0]))
+    print(f"SINGLE USER / Статус кода: {code[0]}")
+    browser.element("li[data-id='unknown']").perform(command.js.scroll_into_view).click()
+    browser.element(response_code).should(have.exact_text(code[0]))
+    print(f"LIST RESOURCE /Статус кода: {code[0]}")
 
-def test_negative(browser_setup):
-    status_code = "404"
+
+
+
+
+def test_code_invalid(browser_setup):
     browser.open(url)
     browser.element("li[data-id='users-single-not-found']").click()
-    browser.element(".response-code.bad").should(have.text(status_code))
-    print(response)
+    browser.element(".response-code.bad").should(have.text(code[2]))
+    print(f"SINGLE USER NOT FOUND / Статус кода: {code[2]}")
+    browser.element("li[data-id='unknown-single-not-found']").perform(command.js.scroll_into_view).click()
+    browser.element(response_code).should(have.exact_text(code[2]))
+    print(f"Статус кода: {code[2]}")
 
 
 
